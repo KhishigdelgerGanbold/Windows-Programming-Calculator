d377ab55d966a69a4304aa66270f06ecb60fa773
Khishigdelger Ganbold <weskera428@gmail.com>
2021-03-12 04:45:14 +0800

    Created class "Memory"

---
+++ WindowsFormsApp_Calculator/Memory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Memory
+    {
+        public double Value { get; set; }
+
+        public Memory(double value)
+        {
+            this.Value = value;
+        }
+
+        public void MAdd(double value)
+        {
+            this.Value += value;
+        }
+
+        public void MSub(double value)
+        {
+            this.Value -= value;
+        }
+
+        public void MClear()
+        {
+            this.Value = 0;
+        }
+    }
+}
